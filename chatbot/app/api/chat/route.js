dd8d8eec1cafeb305ec079bc32e971e0d380e594
Eric Reizas <tuo17224@temple.edu>
2024-08-06 21:23:47 -0400

    Tested out retrieval of raw data from POST request header

--- chatbot/app/api/chat/route.js
+++ chatbot/app/api/chat/route.js
@@ -1,5 +1,6 @@
 import {NextResponse} from 'next/server'
-export function POST(req){
-    console.log('POST /api/chat')
+export async function POST(req){
+    const data = await req.json()
+    console.log(data)
     return NextResponse.json({message:"Hello from the server!"})
 }
