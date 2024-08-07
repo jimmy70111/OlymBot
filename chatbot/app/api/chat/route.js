1c153196e9b0430f20b2f0dbdc6864eaf70f6883
Eric Reizas <tuo17224@temple.edu>
2024-08-07 10:49:33 -0400

    Added message encoder

--- chatbot/app/api/chat/route.js
+++ chatbot/app/api/chat/route.js
@@ -2,5 +2,24 @@ import {NextResponse} from 'next/server'
 export async function POST(req){
     const data = await req.json()
     console.log(data)
-    return NextResponse.json({message:"Hello from the server!"})
+    const stream = new ReadableStream({
+        async start(Controller){
+            const encoder = new TextEncoder()
+            try{
+                for await (const chunk of completion){
+                    const content = chunk.choices[0]?.delta?.content
+                    if (content){
+                        const text = encoder.encode(content)
+                        Controller.enqueue(text)
+                    }
+                }
+            }catch(err){
+                Controller.error(err)
+            }finally{
+                Controller.close()
+            }
+            
+        }
+    })
+    return new NextResponse.json({message:"Hello from the server!"})
 }
